aa80687a548b1e4a33fd76a4226ff3b9da14fdf7
ReniAurelia-2226250028 <reniaurelia_2226250028@mhs.mdp.ac.id>
2023-10-13 18:36:20 +0700

    ReniAurelia_PisahWidget

--- ReniAurelia.dart
+++ ReniAurelia.dart
@@ -7,18 +7,18 @@ void main(){
 class MyApp extends StatelessWidget {
     const MyApp({super.key});
 
+class FirstScreen extends StatelessWidget {
+    const FirstScreen({super.key});
+
     @override
     Widget build (BuildContext context){
-        return MaterialApp(
-            title: "Hallo Widget",
-            theme: ThemeData(primarySwatch: colors.pink),
-            home: Scaffold(
-                appBar: appBar(
+        return Scaffold(
+            appBar: appBar(
                 title: const text("Reni Aurelia"),
             ), //AppBar
             body: const Center(
                 child: Text("Hallo Reni Aurelia"),
             ), //center
-        )); //Scaffold//MaterialApp
-    }
+        ); //Scaffold
+    }
 }
