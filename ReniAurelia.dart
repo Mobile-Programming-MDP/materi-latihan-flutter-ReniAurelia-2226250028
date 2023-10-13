ab6e7d608d53e1b9ca7328b9975a9ccab7a6b3c0
ReniAurelia-2226250028 <reniaurelia_2226250028@mhs.mdp.ac.id>
2023-10-13 18:29:46 +0700

    ReniAurelia

--- ReniAurelia.dart
+++ ReniAurelia.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+    runApp(const myApp());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "Hallo Widget",
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: Scaffold(
-            appBar: AppBar(
-                title: const Text("Reni Aurelia"),
-            ), //Appbar
-        body: const Center(
-            child: Text("Hallo Reni Aurelia"),
-        ), //center
-    ); //scaffold
+class MyApp extends StatelessWidget {
+    const MyApp({super.key});
+
+    @override
+    Widget build (BuildContext context){
+        return MaterialApp(
+            title: "Hallo Widget",
+            theme: ThemeData(primarySwatch: colors.pink),
+            home: Scaffold(
+                appBar: appBar(
+                title: const text("Reni Aurelia"),
+            ), //AppBar
+            body: const Center(
+                child: Text("Hallo Reni Aurelia"),
+            ), //center
+        )); //Scaffold//MaterialApp
+    }
 }
