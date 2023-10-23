ce81621870eb905c144ffecdff38771ac1f333ef
ReniAurelia-2226250028 <reniaurelia_2226250028@mhs.mdp.ac.id>
2023-10-23 13:22:58 +0700

    WisataCandi_ReniAurelia_23Okt

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
-import 'package:wisata_candi/models/candi.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
 
 void main() {
@@ -20,6 +19,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: DetailScreen(candi: candiList[0]),
-    );
+  );
   }
 }
