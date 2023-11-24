a1051ce727cc4f5d8bf7e94ea772524c95a715b1
ReniAurelia-2226250028 <reniaurelia_2226250028@mhs.mdp.ac.id>
2023-11-24 14:39:04 +0700

    Pertemuan 20

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/screens/favorite_screen.dart';
 import 'package:wisata_candi/screens/home_screen.dart';
 // import 'package:wisata_candi/data/candi_data.dart';
 // import 'package:wisata_candi/screens/detail_screen.dart';
@@ -27,16 +28,90 @@ class MyApp extends StatelessWidget {
           ),
         ),
         colorScheme:
-        ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+            ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
           primary: Colors.deepPurple,
           surface: Colors.deepPurple[50],
         ),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      //home: HomeScreen(),
       //home: const SearchScreen(),
       //home: SignInScreen(),
       //home: DetailScreen(candi: candiList[0]),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  //TOD: 1 Deklarasikan variabel
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomeScreen(),
+    SearchScreen(),
+    FavoriteScreen(),
+    ProfileScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //TODO: 2 Buat properti body berupa widget yang ditampilkan
+      body: _children[_currentIndex],
+      //TODO: 3 Buat properti bottonNavigationBar dengan nilai Theme
+      bottomNavigationBar: Theme(
+        //TODO: 4 Buat data dan child dari Theme
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.deepPurple[50],
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.deepPurple,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: Colors.deepPurple,
+              ),
+              label: 'Seacrh',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.deepPurple,
+              ),
+              label: 'Favorite',
+            ),
+             BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: Colors.deepPurple,
+              ),
+              label: 'Person',
+            ),
+          ],
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.deepPurple[100],
+          showUnselectedLabels: true,
+        ),
+      ),
     );
   }
 }
